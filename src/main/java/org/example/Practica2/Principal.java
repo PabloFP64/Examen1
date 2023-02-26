2f9f1939ee69fc1ce82f15efdf889ab24fcc206c
PabloFP64 <63953769+PabloFP64@users.noreply.github.com>
2023-02-26 20:37:05 +0100

    2 terminado

--- src/main/java/org/example/Practica2/Principal.java
+++ src/main/java/org/example/Practica2/Principal.java
@@ -8,22 +8,28 @@ public class Principal{
         {
             Tablero tablero = new Tablero();
             System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
-                    tablero.leerEstadoActual();
-            System.out.println(tablero);
+            tablero.leerEstadoActual();
+            System.out.println(tablero.toString());
             for(int i = 0; i <= 5; i++)
             {
                 TimeUnit.SECONDS.sleep(1);
                 tablero.transitarAlEstadoSiguiente();
-                System.out.println(tablero);
+                //System.out.println(tablero.toString());
+                tablero.leerEstadoActual();
+                System.out.println("--------------------------------------------------");
             }
             System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
                     tablero.generarEstadoActualPorMontecarlo();
-            System.out.println(tablero);
+            //System.out.println(tablero);
+            tablero.leerEstadoActual();
+            System.out.println("--------------------------------------------------");
             for(int i = 0; i <= 15; i++)
             {
                 TimeUnit.SECONDS.sleep(1);
                 tablero.transitarAlEstadoSiguiente();
-                System.out.println(tablero);
+                //System.out.println(tablero.toString());
+                tablero.leerEstadoActual();
+                System.out.println("--------------------------------------------------");
             }
         }catch(InterruptedException e)
         {
--- src/main/java/org/example/Practica2/Tablero.java
+++ src/main/java/org/example/Practica2/Tablero.java
@@ -1,12 +1,27 @@
 package org.example.Practica2;
 
+import java.util.Random;
+
 public class Tablero{
     private static int DIMENSION = 30;
     private int[][] estadoActual; //matriz que representa el
     // estado actual.
     private int[][] estadoSiguiente
             = new int[DIMENSION][DIMENSION]; // Matriz que representa el estado siguiente.
-public void leerEstadoActual(){}
+public void leerEstadoActual(){
+    for (int i = 0; i < DIMENSION; i++) {
+        for (int j = 0; j < DIMENSION; j++) {
+            if (estadoSiguiente[i][j] == 1) {
+                System.out.print("X");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+    System.out.println();
+}
+
 // La secuencia de ceros y unos del fichero es guardada en ‘estadoActual‘ y, utilizando las reglas del juego de la vida, se insertan los ceros y unos correspondientes en ‘estadoSiguiente‘.
     /********************************************************
      * Genera un estado inicial aleatorio. Para cada celda
@@ -14,13 +29,62 @@ public void leerEstadoActual(){}
      * el número es menor que 0,5, entonces la celda está
      * inicialmente viva. En caso contrario, está muerta.
      *******************************************************/
-    public void generarEstadoActualPorMontecarlo(){}
+    public void generarEstadoActualPorMontecarlo(){
+        Random rand = new Random();
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                estadoSiguiente[i][j] = rand.nextInt(2);
+
+            }
+        }
+        //leerEstadoActual();
+    }
 // La secuencia de ceros y unos generada es guardada en ‘estadoActual‘ y, utilizando las reglas del juego de la vida, se insertan los ceros y unos correspondientes en ‘estadoSiguiente‘.
     /********************************************************
      * Transita al estado siguiente según las reglas del
      * juego de la vida.
      ********************************************************/
-    public void transitarAlEstadoSiguiente(){}
+    public void transitarAlEstadoSiguiente(){
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                int vecinos = 0;
+                if (i > 0 && j > 0 && estadoSiguiente[i - 1][j - 1] == 1) {
+                    vecinos++;
+                }
+                if (i > 0 && estadoSiguiente[i - 1][j] == 1) {
+                    vecinos++;
+                }
+                if (i > 0 && j < DIMENSION - 1 && estadoSiguiente[i - 1][j + 1] == 1) {
+                    vecinos++;
+                }
+                if (j > 0 && estadoSiguiente[i][j - 1] == 1) {
+                    vecinos++;
+                }
+                if (j < DIMENSION - 1 && estadoSiguiente[i][j + 1] == 1) {
+                    vecinos++;
+                }
+                if (i < DIMENSION - 1 && j > 0 && estadoSiguiente[i + 1][j - 1] == 1) {
+                    vecinos++;
+                }
+                if (i < DIMENSION - 1 && estadoSiguiente[i + 1][j] == 1) {
+                    vecinos++;
+                }
+                if (i < DIMENSION - 1 && j < DIMENSION - 1 && estadoSiguiente[i + 1][j + 1] == 1) {
+                    vecinos++;
+                }
+                if (estadoSiguiente[i][j] == 1) {
+                    if (vecinos < 2 || vecinos > 3) {
+                        estadoSiguiente[i][j] = 0;
+                    }
+                } else {
+                    if (vecinos == 3) {
+                        estadoSiguiente[i][j] = 1;
+                    }
+                }
+            }
+        }
+        //leerEstadoActual();
+    }
 // La variable ‘estadoActual‘ pasa a tener el contenido de ‘estadoSiguiente‘ y, éste útimo atributo pasar a reflejar el siguiente estado.
     /*******************************************************
      * Devuelve, en modo texto, el estado actual.
@@ -28,7 +92,19 @@ public void leerEstadoActual(){}
      *******************************************************/
     @Override
     public String toString(){
-        return ""; // Esta línea hay que modificarla.
+        String estado = "";
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (estadoSiguiente[i][j] == 1) {
+                    estado += "X";
+                } else {
+                    estado += " ";
+                }
+            }
+            estado += "";
+
+    }
+    return estado;
     }
 }
 
